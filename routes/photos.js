1569d79f39f1cf78c453a241406787265bdfcff9
Lionel-Feuga <feuga.lionel1@gmail.com>
2025-05-20 12:33:41 +0200

    routes photos get delete & delete all

--- routes/photos.js
+++ routes/photos.js
@@ -1,8 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const Photos = require('../models/photos');
 
-router.get('/', (req, res) => {
+router.get('/:userId', async (req, res) => {
+  const userId = req.params.userId;
+  const photos = await Photos.find({ userId: userId });
+  if (!photos) {
+    return res.status(404).json({ message: 'Photos not found' });
+  }
   res.json({
-    message: 'Photos API is working!'
+    result: true,
+    photos
   });
 });
+
+router.delete('/:photoId', async (req, res) => {
+  const photoId = req.params.photoId;
+  const photo = await Photos.findByIdAndDelete(photoId);
+  if (!photo) {
+    return res.status(404).json({ message: 'Photo not found' });
+  }
+  res.json({
+    result: true,
+    message: 'Photo deleted successfully'
+  });
+}); 
+
+router.delete('/deleteAll/:userId', async (req, res) => {
+  const userId = req.params.userId;
+  const photos = await Photos.deleteMany({ userId: userId });
+  if (!photos) {
+    return res.status(404).json({ message: 'Photos not found' });
+  }
+  res.json({
+    result: true,
+    message: 'All photos deleted successfully'
+  });
+})
