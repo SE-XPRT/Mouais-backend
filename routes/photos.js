79210f8db368b58d1c39938d41a1242b3f5b45d5
Cecilia <cecilia.paulo@gmail.com>
2025-05-20 17:06:31 +0200

    o commit

--- routes/photos.js
+++ routes/photos.js
@@ -1,39 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Photos = require('../models/photos');
+const Photos = require("../models/photos");
 
-router.get('/:userId', async (req, res) => {
+router.get("/:userId", async (req, res) => {
   const userId = req.params.userId;
   const photos = await Photos.find({ userId: userId });
   if (!photos) {
-    return res.status(404).json({ message: 'Photos not found' });
+    return res.status(404).json({ message: "Photos not found" });
   }
   res.json({
     result: true,
-    photos
+    photos,
   });
 });
 
-router.delete('/:photoId', async (req, res) => {
+router.delete("/:photoId", async (req, res) => {
   const photoId = req.params.photoId;
   const photo = await Photos.findByIdAndDelete(photoId);
   if (!photo) {
-    return res.status(404).json({ message: 'Photo not found' });
+    return res.status(404).json({ message: "Photo not found" });
   }
   res.json({
     result: true,
-    message: 'Photo deleted successfully'
+    message: "Photo deleted successfully",
   });
-}); 
+});
 
-router.delete('/deleteAll/:userId', async (req, res) => {
+router.delete("/deleteAll/:userId", async (req, res) => {
   const userId = req.params.userId;
   const photos = await Photos.deleteMany({ userId: userId });
   if (!photos) {
-    return res.status(404).json({ message: 'Photos not found' });
+    return res.status(404).json({ message: "Photos not found" });
   }
   res.json({
     result: true,
-    message: 'All photos deleted successfully'
+    message: "All photos deleted successfully",
   });
-})
+});
